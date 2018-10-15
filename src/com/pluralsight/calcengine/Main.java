e0bc0613fc5322a2a2a6185cde0467e716098b2e
Samantha Essel-Addo <sesseladdo@gbbn.com>
2018-10-15 15:25:45 -0400

    CalcEngine with classes and methods.

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,46 +3,34 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-      double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-      double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-      char[] opCodes = {'d', 'a', 's', 'm'};
-      double[] results = new double[opCodes.length];
-
-      for (int i = 0; i < opCodes.length; i++) {
-        switch (opCodes[i]) {
-          case 'a':
-            results[i] = leftVals[i] + rightVals[i];
-            break;
-          case 's':
-            results[i] = leftVals[i] - rightVals[i];
-            break;
-          case 'd':
-            results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-            break;
-        /*
-         * The above line is basically saying...
-         ///
-           if(rightVals[i] !=0.0d)
-          results[i] = leftVals[i] / rightVals[i];
-        else results[i] = 0.0d;
-        ///
-         */
-          case 'm':
-            results[i] = leftVals[i] * rightVals[i];
-            break;
-          default:
-            System.out.println("Error - invalid opCode");
-            break;
+//      double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+//      double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+//      char[] opCodes = {'d', 'a', 's', 'm'};
+//      double[] results = new double[opCodes.length];
 
 
-        }
-      }
+      MathEquation[] equations = new MathEquation[4];
+      equations[0] = create(100.0d,50.0d, 'd');
+      equations[1] = create(25.0d,92.0d, 'a');
+      equations[2] = create(225.0d,17.0d, 's');
+      equations[3] = create(11.0d,3.0d, 'm');
+
         //the line below is a foreach statement like c# but uses ":" instead of "in"
         //So in english it reads "For each theResult (individual result) in results print... " result = (theResult)"
-        for (double theResult : results) {
+        for(MathEquation equation : equations) {
+          equation.execute();
           System.out.print("results = ");
-          System.out.println((theResult));
+          System.out.println((equation.result));
         }
+      }
+
+      public static MathEquation create(double leftVal, double rightVal, char opCode){
+      MathEquation equation = new MathEquation();
+      equation.leftVal = leftVal;
+      equation.rightVal = rightVal;
+      equation.opCode = opCode;
+
+      return equation;
       }
     }
 
--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -6,4 +6,35 @@ public class MathEquation {
   public double rightVal;
   public char opCode;
   public double result;
+
+  public void execute(){
+
+    switch (opCode) {
+      case 'a':
+        result = leftVal + rightVal;
+        break;
+      case 's':
+        result = leftVal - rightVal;
+        break;
+      case 'd':
+        result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+        break;
+        /*
+         * The above line is basically saying...
+         ///
+           if(rightVal !=0.0d)
+          result = leftVals[i] / rightVals[i];
+        else results[i] = 0.0d;
+        ///
+         */
+      case 'm':
+        result = leftVal * rightVal;
+        break;
+      default:
+        System.out.println("Error - invalid opCode");
+        break;
+
+
+    }
+  }
 }
